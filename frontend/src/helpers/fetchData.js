d252d068a73a83b609d5bf9f80778a8696452a61
Jeremiah Okwuolisa <jerryolisa@gmail.com>
2025-05-16 12:39:53 -0700

    test run

--- frontend/src/helpers/fetchData.js
+++ frontend/src/helpers/fetchData.js
@@ -1,4 +1,4 @@
 // export const fetchAllFromEndPoint = (endpoint) => {
 //   const baseUrl = import.meta.env?.VITE_BASE_SERVER_URL || process.env.VITE_BASE_SERVER_URL;
 //   return `${baseUrl}/${endpoint}`;
-// };
+// };    
