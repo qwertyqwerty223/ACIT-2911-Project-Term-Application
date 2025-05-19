e8065dd86a12998416269200e74576773d3d29ba
Jeremiah Okwuolisa <jerryolisa@gmail.com>
2025-05-19 16:51:31 -0700

    changed cors origin

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions ={
-    origin:[
-    'http://localhost:5173',
-    'https://projecttracker-frontend.onrender.com'
-    ], 
+    origin:'https://projecttracker-frontend.onrender.com', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
