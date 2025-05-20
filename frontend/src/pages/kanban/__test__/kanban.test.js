ac34bd04bb43f740a26801f2f6c0a5ac8400bafa
Jeremiah Okwuolisa <jerryolisa@gmail.com>
2025-05-20 03:17:53 -0700

    bugs fix

--- frontend/src/pages/kanban/__test__/kanban.test.js
+++ frontend/src/pages/kanban/__test__/kanban.test.js
@@ -1,118 +1,118 @@
-// import {
-//   fetchTasks,
-//   deleteTask,
-//   mapTasksToLanes,
-//   updateStatusOnCardDrag,
-//   AddTaskCard,
-// } from "../kanban"; // Adjust path if needed
-// import axios from "axios"; // Axios import
-// import { waitFor } from "@testing-library/react"; // React testing utilities
+import {
+  fetchTasks,
+  deleteTask,
+  mapTasksToLanes,
+  updateStatusOnCardDrag,
+  AddTaskCard,
+} from "../kanban"; // Adjust path if needed
+import axios from "axios"; // Axios import
+import { waitFor } from "@testing-library/react"; // React testing utilities
 
-// // Mock axios to control API behaviour
-// jest.mock("axios");
-// // mock SVG asset to avoid file-loader issues
-// jest.mock("../../../assets/delete-icon.svg", () => "delete-icon.svg");
-// jest.mock("../../../assets/edit-icon.svg", () => "edit-icon.svg");
+// Mock axios to control API behaviour
+jest.mock("axios");
+// mock SVG asset to avoid file-loader issues
+jest.mock("../../../assets/delete-icon.svg", () => "delete-icon.svg");
+jest.mock("../../../assets/edit-icon.svg", () => "edit-icon.svg");
 
-// // Test Block for fetching tasks
-// describe("fetchTasks", () => {
+// Test Block for fetching tasks
+describe("fetchTasks", () => {
 
-//   // Test Case #1: Successful task fetching
-//   it("should fetch tasks and update data correctly", async () => {
-//     // Create the shape of mock data for axios.get
-//     const mockData = [
-//       { _id: "1", description: "Test Task", status: "planned", user: ["John"] },
-//     ];
+  // Test Case #1: Successful task fetching
+  // it("should fetch tasks and update data correctly", async () => {
+  //   // Create the shape of mock data for axios.get
+  //   const mockData = [
+  //     { _id: "1", description: "Test Task", status: "planned", user: ["John"] },
+  //   ];
 
-//     // mock axios.get to resolve with the mock data
-//     axios.get.mockResolvedValue({ data: mockData });
+  //   // mock axios.get to resolve with the mock data
+  //   axios.get.mockResolvedValue({ data: mockData });
 
-//     // mock state setter
-//     const setData = jest.fn();
+  //   // mock state setter
+  //   const setData = jest.fn();
 
-//     // Calling fetchTasks with state setter mock
-//     await fetchTasks(setData);
+  //   // Calling fetchTasks with state setter mock
+  //   await fetchTasks(setData);
 
-//     // Check if setData was called with the correct updated data
-//     await waitFor(() =>
-//       // assert that setData was called with correct data structure
-//       expect(setData).toHaveBeenCalledWith({
-//         // verify lanes array to contain specific objects
-//         lanes: expect.arrayContaining([
-//           // verify object to have specific properties
-//           expect.objectContaining({
-//             id: "lane1",
-//             title: "Planned",
-//             // verify card array to contain the expected objects
-//             cards: expect.arrayContaining([
-//               // verify the object is the one with title and description keys
-//               expect.objectContaining({
-//                 title: "Unnamed Task",
-//                 description: "Test Task",
-//               }),
-//             ]),
-//           }),
-//         ]),
-//       })
-//     );
-//   });
+  //   // Check if setData was called with the correct updated data
+  //   await waitFor(() =>
+  //     // assert that setData was called with correct data structure
+  //     expect(setData).toHaveBeenCalledWith({
+  //       // verify lanes array to contain specific objects
+  //       lanes: expect.arrayContaining([
+  //         // verify object to have specific properties
+  //         expect.objectContaining({
+  //           id: "lane1",
+  //           title: "Planned",
+  //           // verify card array to contain the expected objects
+  //           cards: expect.arrayContaining([
+  //             // verify the object is the one with title and description keys
+  //             expect.objectContaining({
+  //               title: "Unnamed Task",
+  //               description: "Test Task",
+  //             }),
+  //           ]),
+  //         }),
+  //       ]),
+  //     })
+  //   );
+  // });
 
-//   // Test Case #2: handle task fetch errors
-//   it("should handle error when fetching tasks fails", async () => {
-//     // Mocking axios GET request to reject with an error
-//     axios.get.mockRejectedValue(new Error("Failed to fetch tasks"));
+  // Test Case #2: handle task fetch errors
+  it("should handle error when fetching tasks fails", async () => {
+    // Mocking axios GET request to reject with an error
+    axios.get.mockRejectedValue(new Error("Failed to fetch tasks"));
 
-//     // define mock for state setter
-//     const setData = jest.fn();
+    // define mock for state setter
+    const setData = jest.fn();
 
-//     // Calling fetchTasks with state setter mock
-//     await fetchTasks(setData);
+    // Calling fetchTasks with state setter mock
+    await fetchTasks(setData);
 
-//     // Verifying that setData is not called when the API call fails
-//     await waitFor(() => expect(setData).not.toHaveBeenCalled());
-//   });
-// });
+    // Verifying that setData is not called when the API call fails
+    await waitFor(() => expect(setData).not.toHaveBeenCalled());
+  });
+});
 
-// // Test block for task deletion (success + failure)
-// describe("deleteTasks", () => {
-//   // Test Case #3: Success task deletion
-//   it("should delete task successfully", async () => {
-//     // mock the axios DELETE request to resolve with empty object
-//     axios.delete.mockResolvedValue({});
+// Test block for task deletion (success + failure)
+describe("deleteTasks", () => {
+  // Test Case #3: Success task deletion
+  // it("should delete task successfully", async () => {
+  //   // mock the axios DELETE request to resolve with empty object
+  //   axios.delete.mockResolvedValue({});
 
-//     // mock the callback function 'onTaskDeleted'
-//     const onTaskDeleted = jest.fn();
+  //   // mock the callback function 'onTaskDeleted'
+  //   const onTaskDeleted = jest.fn();
 
-//     // call the deleteTask for success scenario
-//     await deleteTask("1", onTaskDeleted);
+  //   // call the deleteTask for success scenario
+  //   await deleteTask("1", onTaskDeleted);
 
-//     // test for if the correct API URL is being called
-//     expect(axios.delete).toHaveBeenCalledWith("https://projecttracker-pac8.onrender.com/tasks/1");
+  //   // test for if the correct API URL is being called
+  //   expect(axios.delete).toHaveBeenCalledWith("https://projecttracker-pac8.onrender.com/tasks/1");
 
-//     // verify that the onTaskDeleted was called
-//     expect(onTaskDeleted).toHaveBeenCalledTimes(1);
-//   });
+  //   // verify that the onTaskDeleted was called
+  //   expect(onTaskDeleted).toHaveBeenCalledTimes(1);
+  // });
 
-//   // Test Case #4: handling deletion failure
-//   it("Should handle error if deletion fails", async () => {
-//     // mock the axios.delete for failure
-//     axios.delete.mockRejectedValue(new Error("failed to delete task"));
+  // Test Case #4: handling deletion failure
+  it("Should handle error if deletion fails", async () => {
+    // mock the axios.delete for failure
+    axios.delete.mockRejectedValue(new Error("failed to delete task"));
 
-//     // mock the callback function 'onTaskDeleted'
-//     const onTaskDeleted = jest.fn();
+    // mock the callback function 'onTaskDeleted'
+    const onTaskDeleted = jest.fn();
 
-//     // call the deleteTask method for failure scenario
-//     await deleteTask("1", onTaskDeleted);
+    // call the deleteTask method for failure scenario
+    await deleteTask("1", onTaskDeleted);
 
-//     // test for correct API URL calling
-//     expect(axios.delete).toHaveBeenCalledWith("https://projecttracker-pac8.onrender.com/tasks/1");
+    // test for correct API URL calling
+    expect(axios.delete).toHaveBeenCalledWith("https://projecttracker-pac8.onrender.com/tasks/1");
 
-//     // verify that the onTaskDeleted in NOT called upon error
-//     expect(onTaskDeleted).not.toHaveBeenCalled();
-//   });
-// });
+    // verify that the onTaskDeleted in NOT called upon error
+    expect(onTaskDeleted).not.toHaveBeenCalled();
+  });
+});
 
-// // Test block for mapping tasks to lanes
+// Test block for mapping tasks to lanes
 // describe("mapTasksToLanes", () => {
 //   const initialLanes = [
 //     { id: "lane1", title: "Planned", cards: [] },
@@ -165,45 +165,45 @@
 //   });
 // });
 
-// describe("updateStatusOnCardDrag", () => {
-//   // TEST CASE #7: successful update task status
-//   it("should call axios.put with the correct data to update task status", async () => {
-//     const card = {
-//       id: "1",
-//       description: "Task no. 1",
-//       user: "User1",
-//     };
-//     const newStatus = "Completed";
-//     axios.patch.mockResolvedValue({});
+describe("updateStatusOnCardDrag", () => {
+  // TEST CASE #7: successful update task status
+  // it("should call axios.put with the correct data to update task status", async () => {
+  //   const card = {
+  //     id: "1",
+  //     description: "Task no. 1",
+  //     user: "User1",
+  //   };
+  //   const newStatus = "Completed";
+  //   axios.patch.mockResolvedValue({});
 
-//     await updateStatusOnCardDrag(card, newStatus);
+  //   await updateStatusOnCardDrag(card, newStatus);
 
-//     expect(axios.patch).toHaveBeenCalledWith("https://projecttracker-pac8.onrender.com/tasks/1", {
-//       _id: "1",
-//       description: "Task no. 1",
-//       status: "Completed",
-//       user: ["User1"],
-//     });
-//   });
+  //   expect(axios.patch).toHaveBeenCalledWith("https://projecttracker-pac8.onrender.com/tasks/1", {
+  //     _id: "1",
+  //     description: "Task no. 1",
+  //     status: "Completed",
+  //     user: ["User1"],
+  //   });
+  // });
 
-//   // TEST CASE #8: Handle API error during status update
-//    it("should log an error if axios.put fails", async () => {
-//     const card = {
-//       id: "1",
-//       description: "Task no. 1",
-//       user: "User1",
-//     };
-//     const newStatus = "Completed";
-//     const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(() => {});
-//     axios.patch.mockRejectedValue(new Error("API PUT Error"));
+  // TEST CASE #8: Handle API error during status update
+  //  it("should log an error if axios.put fails", async () => {
+  //   const card = {
+  //     id: "1",
+  //     description: "Task no. 1",
+  //     user: "User1",
+  //   };
+  //   const newStatus = "Completed";
+  //   const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(() => {});
+  //   axios.patch.mockRejectedValue(new Error("API PUT Error"));
 
-//     await updateStatusOnCardDrag(card, newStatus);
+  //   await updateStatusOnCardDrag(card, newStatus);
 
-//     expect(axios.patch).toHaveBeenCalledWith(
-//       "https://projecttracker-pac8.onrender.com/tasks/1",
-//       expect.anything()
-//     );
-//     expect(consoleErrorSpy).toHaveBeenCalledWith("Error Sending tasks:", expect.any(Error));
-//     consoleErrorSpy.mockRestore();
-//   });
-// });
+  //   expect(axios.patch).toHaveBeenCalledWith(
+  //     "https://projecttracker-pac8.onrender.com/tasks/1",
+  //     expect.anything()
+  //   );
+  //   expect(consoleErrorSpy).toHaveBeenCalledWith("Error Sending tasks:", expect.any(Error));
+  //   consoleErrorSpy.mockRestore();
+  // });
+});
--- frontend/src/pages/timeLine/__test__/timeLine.test.js
+++ frontend/src/pages/timeLine/__test__/timeLine.test.js
@@ -1,102 +1,102 @@
-// import { fetchEvents, deleteEvent } from "../timeLine"; // Adjust path if needed
-// import axios from "axios"; // Axios import
-// import { fireEvent, waitFor, render, screen } from "@testing-library/react"; // React testing utilities
-// import React from "react";
+import { fetchEvents, deleteEvent } from "../timeLine"; // Adjust path if needed
+import axios from "axios"; // Axios import
+import { fireEvent, waitFor, render, screen } from "@testing-library/react"; // React testing utilities
+import React from "react";
 
-// // Mock axios functions
-// jest.mock("axios");
+// Mock axios functions
+jest.mock("axios");
 
-// // Mock assets to resolve errors
-// jest.mock("../../../assets/delete-icon.svg", () => "delete-icon.svg");
-// jest.mock("../../../assets/edit-icon.svg", () => "edit-icon.svg");
+// Mock assets to resolve errors
+jest.mock("../../../assets/delete-icon.svg", () => "delete-icon.svg");
+jest.mock("../../../assets/edit-icon.svg", () => "edit-icon.svg");
 
-// // Mock useLocation to provide a dynamic token
-// jest.mock("react-router-dom", () => {
-//   const actual = jest.requireActual("react-router-dom");
-//   return {
-//     ...actual,
-//     useLocation: () => ({
-//       pathname: "/projectName/sectionName/token123",
-//     }),
-//   };
-// });
+// Mock useLocation to provide a dynamic token
+jest.mock("react-router-dom", () => {
+  const actual = jest.requireActual("react-router-dom");
+  return {
+    ...actual,
+    useLocation: () => ({
+      pathname: "/projectName/sectionName/token123",
+    }),
+  };
+});
 
-// // Test block for handling events
-// describe("HandleEvents", () => {
-//   beforeEach(() => {
-//     axios.get.mockClear();
-//   });
+// Test block for handling events
+describe("HandleEvents", () => {
+  beforeEach(() => {
+    axios.get.mockClear();
+  });
 
-//   // TEST CASE #1: Successful data fetching
-//   it("should fetch events and update data correctly", async () => {
-//     const mockData = {
-//       data: {
-//         events: [
-//           {
-//             _id: "681d6627b4675a6ae1708069",
-//             title: "This is a new event",
-//             date: "2025-05-08T00:00:00.000Z",
-//             description: "something something something",
-//           },
-//           {
-//             _id: "681d7422d9b13cc21a4d4753",
-//             title: "another new event",
-//             date: "2025-05-10T00:00:00.000Z",
-//             description: "Lorem ipsum",
-//           },
-//         ],
-//       },
-//     };
+  // TEST CASE #1: Successful data fetching
+  // it("should fetch events and update data correctly", async () => {
+  //   const mockData = {
+  //     data: {
+  //       events: [
+  //         {
+  //           _id: "681d6627b4675a6ae1708069",
+  //           title: "This is a new event",
+  //           date: "2025-05-08T00:00:00.000Z",
+  //           description: "something something something",
+  //         },
+  //         {
+  //           _id: "681d7422d9b13cc21a4d4753",
+  //           title: "another new event",
+  //           date: "2025-05-10T00:00:00.000Z",
+  //           description: "Lorem ipsum",
+  //         },
+  //       ],
+  //     },
+  //   };
 
-//     axios.get.mockResolvedValue(mockData);
+  //   axios.get.mockResolvedValue(mockData);
 
-//     const setTimelineEvent = jest.fn();
-//     const location = { pathname: "/projectName/sectionName/token123" };
+  //   const setTimelineEvent = jest.fn();
+  //   const location = { pathname: "/projectName/sectionName/token123" };
 
-//     await fetchEvents(setTimelineEvent, location);
+  //   await fetchEvents(setTimelineEvent, location);
 
-//     expect(setTimelineEvent).toHaveBeenCalledWith([
-//       {
-//         id: "681d6627b4675a6ae1708069",
-//         title: "This is a new event",
-//         description: "something something something",
-//         date: "5/8/2025",
-//       },
-//       {
-//         id: "681d7422d9b13cc21a4d4753",
-//         title: "another new event",
-//         description: "Lorem ipsum",
-//         date: "5/10/2025",
-//       },
-//     ]);
-//   });
+  //   expect(setTimelineEvent).toHaveBeenCalledWith([
+  //     {
+  //       id: "681d6627b4675a6ae1708069",
+  //       title: "This is a new event",
+  //       description: "something something something",
+  //       date: "5/8/2025",
+  //     },
+  //     {
+  //       id: "681d7422d9b13cc21a4d4753",
+  //       title: "another new event",
+  //       description: "Lorem ipsum",
+  //       date: "5/10/2025",
+  //     },
+  //   ]);
+  // });
 
-//   // TEST CASE #2: Handling fetch errors
-//   it("should handle error when fetching events fails", async () => {
-//     axios.get.mockRejectedValue(new Error("Failed to fetch events"));
+  // TEST CASE #2: Handling fetch errors
+  it("should handle error when fetching events fails", async () => {
+    axios.get.mockRejectedValue(new Error("Failed to fetch events"));
 
-//     const setTimelineEvent = jest.fn();
-//     const location = { pathname: "/projectName/sectionName/token123" };
+    const setTimelineEvent = jest.fn();
+    const location = { pathname: "/projectName/sectionName/token123" };
 
-//     await fetchEvents(setTimelineEvent, location);
+    await fetchEvents(setTimelineEvent, location);
 
-//     await waitFor(() => expect(setTimelineEvent).not.toHaveBeenCalled());
-//   });
+    await waitFor(() => expect(setTimelineEvent).not.toHaveBeenCalled());
+  });
 
-//   // TEST CASE #2: Handling empty response
-//   it("should handle empty events response", async () => {
-//     axios.get.mockResolvedValue({ data: { events: [] } });
+  // TEST CASE #2: Handling empty response
+  it("should handle empty events response", async () => {
+    axios.get.mockResolvedValue({ data: { events: [] } });
 
-//     const setTimelineEvent = jest.fn();
-//     const location = { pathname: "/projectName/sectionName/token123" };
+    const setTimelineEvent = jest.fn();
+    const location = { pathname: "/projectName/sectionName/token123" };
 
-//     await fetchEvents(setTimelineEvent, location);
+    await fetchEvents(setTimelineEvent, location);
 
-//     expect(setTimelineEvent).toHaveBeenCalledWith([]);
-//   });
-// });
+    expect(setTimelineEvent).toHaveBeenCalledWith([]);
+  });
+});
 
-// // Test block for deleting events
+// Test block for deleting events
 // describe("deleteEvents", () => {
 //   // TEST CASE #3: Successful event deletion
 //   it("should delete event successfully", async () => {
